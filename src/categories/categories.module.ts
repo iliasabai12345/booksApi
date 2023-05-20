577fd78411034d101d638bee783b1fafbe8c5f30
iliasabai123 <ilias.abai.alser@gmail.com>
2023-05-20 12:36:07 +0600

    add contents api

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,12 +1,15 @@
 import {Module} from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
+import {BooksService} from "src/books/books.service";
+import {Book, BookSchema} from "src/books/schemas/book.schema";
 import {CategoriesController} from "src/categories/categories.controller";
 import {Category, CategorySchema} from "src/categories/schemas/category.schema";
 import {CategoriesService} from './categories.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: Category.name, schema: CategorySchema}])],
-    providers: [CategoriesService],
+    imports: [MongooseModule.forFeature([{name: Category.name, schema: CategorySchema}]),
+        MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])],
+    providers: [CategoriesService,BooksService],
     controllers: [CategoriesController]
 })
 export class CategoriesModule {
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,12 +1,15 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
+import {BooksService} from "src/books/books.service";
 import {CreateCategoryDto} from "src/categories/dto/create-category.dto";
 import {Category, CategoryDocument} from "src/categories/schemas/category.schema";
+import {BooksTemp} from "src/shared/books/books";
 
 @Injectable()
 export class CategoriesService {
-    constructor(@InjectModel(Category.name) private categoryModel: Model<CategoryDocument>
+    constructor(@InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
+                private readonly booksService: BooksService
     ) {
     }
 
@@ -46,10 +49,29 @@ export class CategoriesService {
         }
     }
 
+    // Получение контента из категорий
+    // todo Метод отстойный потом требует переделки
     async getContents(): Promise<{ code: number, data: Category[], message: string }> {
         try {
+            if (!BooksTemp.data.length) {
+                const books = await this.booksService.getAll();
+                BooksTemp.data = books.data;
+            }
+            const category_keywords = [];
             const categories = await this.categoryModel.find().exec();
-            const data = categories.filter(book => book.is_content);
+            const content_categories = categories.filter(book => book.is_content);
+            content_categories.forEach(category => {
+                !category_keywords.includes(category.keyword) && category_keywords.push(category.keyword);
+            });
+            const data = [];
+            category_keywords.forEach(category => {
+                data.push({
+                    category: category,
+                    title: content_categories.find(res => res.keyword === category).title,
+                    title_kk: content_categories.find(res => res.keyword === category).title_kk,
+                    books: BooksTemp.data.filter(book => book.category === category).splice(0, 4)
+                })
+            })
             return {
                 code: 0,
                 data,
